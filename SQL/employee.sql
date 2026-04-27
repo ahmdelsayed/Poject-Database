a8522b5619ccdca98c92221c61208c80e40416f0
ahmed <amd.ahmedelsayed@gmail.com>
2026-04-27 05:47:16 +0300

    add employee.sql and deleted SELECT top (1000)

---
+++ SQL/employee.sql
@@ -0,0 +1,30 @@
+USE [ims]
+GO
+
+/****** Object:  Table [dbo].[employee]    Script Date: 4/27/2026 5:45:51 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[employee](
+	[eid] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](100) NULL,
+	[email] [varchar](100) NULL,
+	[contact] [varchar](100) NULL,
+	[gender] [varchar](50) NULL,
+	[dob] [varchar](50) NULL,
+	[doj] [varchar](50) NULL,
+	[pass] [varchar](50) NULL,
+	[utype] [varchar](50) NULL,
+	[address] [varchar](max) NULL,
+	[salary] [varchar](50) NULL,
+ CONSTRAINT [PK_employee] PRIMARY KEY CLUSTERED 
+(
+	[eid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
