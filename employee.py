01d6fc071f619966889a124ab7a3afd510af037e
Ahmed ELsayed <amd.ahmedelsayed@gmail.com>
2026-05-06 23:57:05 +0300

    Update employee.py

--- employee.py
+++ employee.py
@@ -149,7 +149,7 @@ class employeeClass:
     #================ Functions =================
     
     def get_connection(self):
-        return pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=AMD\\SQLEXPRESS;DATABASE=ims;Trusted_Connection=yes;TrustServerCertificate=yes;')
+        return pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=***\\SQLEXPRESS;DATABASE=ims;Trusted_Connection=yes;TrustServerCertificate=yes;')
 
     def add(self):
         con = self.get_connection()
